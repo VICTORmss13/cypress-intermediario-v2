21f9cdfe938bcb3269b06ef5bc6e2c8ae95b0856
Victor Mendes <victormendes13@gmail.com>
2023-12-18 17:50:54 -0300

    Fix setLabelOnIssue -- projectname => project.name

--- cypress/e2e/gui/setLabelOnIssue.cy.js
+++ cypress/e2e/gui/setLabelOnIssue.cy.js
@@ -1,3 +1,5 @@
+import { faker } from '@faker-js/faker'
+
 const options = { env: { snapshotOnly: true } }
 
 describe('Set label on issue', options, () => {
@@ -21,7 +23,7 @@ describe('Set label on issue', options, () => {
     cy.api_createIssue(issue)
       .then(response => {
         cy.api_createLabel(response.body.project_id, label)
-        cy.visit(`${Cypress.env('user_name')}/${issue.projectname}/issues/${response.body.iid}`)
+        cy.visit(`${Cypress.env('user_name')}/${issue.project.name}/issues/${response.body.iid}`)
       })
   })
 
