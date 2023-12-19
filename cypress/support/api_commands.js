581f75861e4d6bdb7be084ca24083d75f5ee8083
Victor Mendes <victormendes13@gmail.com>
2023-12-19 14:03:57 -0300

    Criando Milestone

--- cypress/support/api_commands.js
+++ cypress/support/api_commands.js
@@ -44,6 +44,7 @@ Cypress.Commands.add('api_createIssue', issue => {
         headers: { Authorization: accessToken },
       })
     })
+})
 
 Cypress.Commands.add('api_createLabel', (projectId, label) => {
   cy.request({
@@ -57,4 +58,11 @@ Cypress.Commands.add('api_createLabel', (projectId, label) => {
   })
 })
 
+Cypress.Commands.add('api_createMilestone', (projectId, milestone) => {
+  cy.request({
+    method: 'POST',
+    url: `/api/v4/projects/${projectId}/milestones`,
+    body: { title: milestone.title },
+    headers: { Authorization: accessToken },
+  })
 })
